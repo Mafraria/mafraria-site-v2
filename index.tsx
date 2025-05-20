fe547c16ff42c68a5ab309424853fc510606c57e
Macintosh <macintosh@iMac.home>
2025-05-20 15:22:08 +0100

    Commit Mafraria

---
+++ index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Mafraria - Natas & Coffee House</h1>
+      <p>Bem-vindo ao nosso site!</p>
+      <a href="/loja">Ir para a Loja</a>
+    </div>
+  );
+}
---
+++ loja.tsx
@@ -0,0 +1,8 @@
+export default function Loja() {
+  return (
+    <div>
+      <h1>Loja de Cafés</h1>
+      <p>Em breve disponível...</p>
+    </div>
+  );
+}
